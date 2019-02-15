e3af03314e5015626561d5caab13f6b5941246cd
sandeep butte <sandeep030690butte@gmail.com>
2019-02-15 14:01:14 +0530

    behvh# Please enter the commit message for your changes. Lines starting

--- src/components/CampaignList.js
+++ src/components/CampaignList.js
@@ -3,11 +3,18 @@ import React from "react";
 const CampaignList = (props)=>{
     return(
         <div>
+            <ul>
+                <li>Campaign Name</li>
+                <li>Author</li>
+                <li>Created At</li>
+                <li>LastUpdate</li>
+            </ul>
         {props.campaignDtails.map((val,ind)=>(
             <ul key={ind}>
                 <li className="campainname">campaign{ind+1}<span>{val.name}</span></li>
-                <li>{val.createdAt}</li>
                 <li>{val.createdBy}</li>
+                <li>{val.createdAt}</li>
+                {val.UpdatedAt && <li>{val.UpdatedAt}</li>}
                 <li id={ind} onClick={e=>props.handleEdit(e)}>Edit Campaign</li>
                 <li id={ind} onClick={e=>props.handelDelete(e)}>Delete Campaign</li>
                 <li id={ind} >Comment</li>
